031f020c02c7c5166ee6e0b97b5c00b5911c8e90
PLGoUoN <133609554+PLGoUoN@users.noreply.github.com>
2023-05-15 00:28:02 +0100

    Add files via upload

--- survis-master/src/data/generated/bib.js
+++ survis-master/src/data/generated/bib.js
@@ -66,21 +66,6 @@
         "volume": "26",
         "year": "2019"
     },
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
-        "type": "inproceedings",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
-    },
     "Designing for Repair": {
         "abstract": "This paper explores issues that come up in practices of breakage and repair through two projects: the 'XO' laptops of One Laptop Per Child in Paraguay and public sites of facilitated repair in California, USA. Collectively drawing on 15 months of ethnographic fieldwork, 156 interviews, and archival research, we find that breakdown and repair are not processes that designers can effectively script ahead of time; instead, they emerge in everyday practice. These practices are shaped by material, infrastructural, gendered, political, and socioeconomic factors -- such as manufacturing limitations, access to repair parts and expertise, and environmental convictions -- which designers often did not, and may not have been able to, anticipate. We call the material realities and practices of repair negotiated endurance, which is illustrated by four themes from our findings: the negotiated identification of breakdown, collaborative definitions of worth, the fraught nature of collaborative expertise, and the gendered stakes of repair.",
         "address": "New York, NY, USA",
